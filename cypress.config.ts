1154d249cb18f4c5ca44e4839c13c378122f8910
bhimpd <dreamypd73@gmail.com>
2025-05-27 18:58:29 +0545

    added cypress cloud configuration and integrattion

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    projectId: "966vsm",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
